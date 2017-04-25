cbfd73c91ce9988b61580ddd6edaac2ddc54b06d
Kory Skarbek and Melvin Gruschow <kory+melvin@>
2017-04-25 08:46:46 -0700

    add base files and folders, as well as begin configuration of the context model files, and creation of user model have also added the home views and controllers just to test compilation success

---
+++ src/stackclone/Models/StackDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace stackclone.Models
+{
+    public class StackDbContext : DbContext
+    {
+        public StackDbContext()
+        {
+
+        }
+
+    }
+
+    public class UserDbContext : IdentityDbContext<User>
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
